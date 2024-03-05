478db9c427662d5507bcac5d4f2d1eddc20936d2
고세종 <fulfilled7418@gmail.com>
2024-03-05 21:56:52 +0900

    feat: 로그인 기능 구현

--- 1-team-front/src/components/LogIn/LogIn.jsx
+++ 1-team-front/src/components/LogIn/LogIn.jsx
@@ -1,5 +1,9 @@
 /* eslint-disable */
+import { useState } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
+import { useMutation } from 'react-query';
+import { useNavigate } from 'react-router-dom';
 
 const LogInForm = styled.form`
   padding: 40px 30px;
@@ -66,22 +70,68 @@ const FooterBox = styled.div`
 `;
 
 const LogIn = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isError, setIsError] = useState(false);
+
+  const fetchLogin = async (data) => {
+    const url = 'http://localhost:8080/login';
+    const payload = data;
+    return await axios.post(url, payload);
+  };
+
+  const submitLogin = useMutation(fetchLogin, {
+    onSuccess: (res) => {
+      setIsError(false);
+      localStorage.clear();
+      localStorage.setItem('email', res.data.email);
+      localStorage.setItem('token', res.data.token);
+      navigate('/');
+    },
+    onError: () => {
+      setIsError(true);
+    },
+  });
+
+  const chageInputHandler = (value, setFn) => {
+    setFn(value);
+  };
+
+  const loginSubmitHander = (event) => {
+    event.preventDefault();
+    submitLogin.mutate({ email: email, password: password });
+  };
+
   return (
-    <LogInForm>
+    <LogInForm onSubmit={loginSubmitHander}>
       <h2>환영합니다 !</h2>
       <InputBox>
         <label htmlFor="email">
           이메일
           <span> *</span>
         </label>
-        <input id="email" type="text"></input>
+        <input
+          id="email"
+          type="text"
+          value={email}
+          onChange={(event) => chageInputHandler(event.target.value, setEmail)}
+        ></input>
       </InputBox>
       <InputBox>
         <label htmlFor="password">
           비밀번호
           <span> *</span>
         </label>
-        <input id="password" type="password"></input>
+        <input
+          id="password"
+          type="password"
+          value={password}
+          onChange={(event) =>
+            chageInputHandler(event.target.value, setPassword)
+          }
+        ></input>
+        {isError && <p>이메일 또는 비밀번호가 일치하지 않습니다.</p>}
         <span>비밀번호를 잊으셨나요 ?</span>
       </InputBox>
       <LogInButton type="submit">로그인</LogInButton>
