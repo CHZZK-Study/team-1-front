b076c4ca92cff31db5b938403d14e3ce9004b9a2
고세종 <fulfilled7418@gmail.com>
2024-03-05 20:55:45 +0900

    feat: react-router-dom 사용 화면 전환으로 인한 상태 제거

--- 1-team-front/src/stores/Auth/auth.js
+++ 1-team-front/src/stores/Auth/auth.js
@@ -1,12 +1,6 @@
 import { create } from 'zustand';
 
 const useAuthStore = create((set) => ({
-  isLogIn: false,
-  setIsLogIn: (isLogIn) => set({ isLogIn }),
-  isSignUp: true,
-  setIsSignUp: (isSignUp) => set({ isSignUp }),
-  isEmailAuth: false,
-  setIsEmailAuth: (isEmailAuth) => set({ isEmailAuth }),
   authForm: { email: '', password: '', nickname: '' },
   setAuthForm: (authForm) => set(authForm),
 }));
