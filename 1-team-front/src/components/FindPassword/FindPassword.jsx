142336a809476e50b611aca0527b2f09ce3853d2
고세종 <fulfilled7418@gmail.com>
2024-03-06 16:32:52 +0900

    feat: 비밀번호 찾기 기능 구현

--- 1-team-front/src/components/FindPassword/FindPassword.jsx
+++ 1-team-front/src/components/FindPassword/FindPassword.jsx
@@ -1,5 +1,8 @@
 /*eslint-disable*/
 
+import axios from 'axios';
+import { useState } from 'react';
+import { useMutation } from 'react-query';
 import styled from 'styled-components';
 
 const FindPasswordForm = styled.form`
@@ -40,7 +43,7 @@ const InputBox = styled.div`
   & p {
     font-size: 0.8rem;
     font-weight: 600;
-    color: #ed4245;
+    color: ${(props) => (props.isSubmit ? '#00aff4' : '#ed4245')};
   }
 `;
 const SubmitButton = styled.button`
@@ -53,13 +56,52 @@ const SubmitButton = styled.button`
 `;
 
 const FindPassword = () => {
+  const [email, setEmail] = useState('');
+  const [isError, setIsError] = useState(false);
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const fetchFindPassword = async (data) => {
+    const url = 'http://localhost:8080/member/password-reissue';
+    const payload = data;
+    return await axios.post(url, payload);
+  };
+
+  const submitFindPassword = useMutation(fetchFindPassword, {
+    onSuccess: () => {
+      setIsError(false);
+      setIsSubmit(true);
+    },
+    onError: () => {
+      setIsSubmit(false);
+      setIsError(true);
+    },
+  });
+
+  const emailInputChangeHandler = (value, setFn) => {
+    setFn(value);
+  };
+
+  const findPasswordSubmitHandler = (event) => {
+    event.preventDefault();
+    submitFindPassword.mutate({ email: email });
+  };
   return (
-    <FindPasswordForm>
+    <FindPasswordForm onSubmit={findPasswordSubmitHandler}>
       <h2>비밀번호를 잊으셨나요 ?</h2>
       <p>입력하신 이메일로 비밀번호 재설정 링크를 보내드립니다.</p>
-      <InputBox>
+      <InputBox isSubmit={isSubmit}>
         <label>이메일</label>
-        <input type="text"></input>
+        <input
+          type="text"
+          value={email}
+          onChange={(event) =>
+            emailInputChangeHandler(event.target.value, setEmail)
+          }
+        ></input>
+        {isError && <p>이메일이 유효하지 않습니다.</p>}
+        {isSubmit && (
+          <p>입력하신 이메일로 비밀번호 재설정 링크를 보냈습니다.</p>
+        )}
       </InputBox>
       <SubmitButton>확인</SubmitButton>
     </FindPasswordForm>
